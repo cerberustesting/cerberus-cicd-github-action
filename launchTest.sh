21c8bf6c696d0fd046a721c748b209d35d619a62
Benoit CIVEL <bcivel@users.noreply.github.com>
2022-06-01 23:10:50 +0200

    Add country, Environment, Robot and Tag parameters

--- launchTest.sh
+++ launchTest.sh
@@ -1,24 +1,64 @@
 #!/bin/bash
 
 ###Get CampaignName and Cerberus Host
-while getopts a:h:c:k: flag
+while getopts a:h:c:k:C:E:R:T: flag
 do
     case "${flag}" in
         a) AUTHOR=${OPTARG};;
         h) HOST=${OPTARG};;
         c) CAMPAIGN=${OPTARG};;
         k) APIKEY=${OPTARG};;
+        C) set -f
+           IFS=,
+           COUNTRY=($OPTARG);;
+        E) set -f
+           IFS=,
+           ENVIRONMENT=($OPTARG);;
+        R) set -f
+           IFS=,
+           ROBOT=($OPTARG);;
+        T) TAG=${OPTARG};;
     esac
 done
+
+EXTRA_ARGS=()
 echo "Author: $AUTHOR";
 echo "Cerberus Host: $HOST";
 echo "Campaign Name: $CAMPAIGN";
 
-###Generate Tag using Campaign Name, Commiter and UnixTimestamp
-tag=$CAMPAIGN.$AUTHOR.$(date +%s)
+if [[ "$COUNTRY" != "" ]]; then
+    echo "Override Country: ${COUNTRY[@]}";
+    for i in "${COUNTRY[@]}"; do
+      EXTRA_ARGS+=(-d "country=${i}")
+    done
+fi
+
+if [[ "$ENVIRONMENT" != "" ]]; then
+    echo "Override Environment: ${ENVIRONMENT[@]}";
+    for i in "${ENVIRONMENT[@]}"; do
+      EXTRA_ARGS+=(-d "environment=${i}")
+    done
+fi
+if [[ "$ROBOT" != "" ]]; then
+    echo "Override Robot: ${ROBOT[@]}";
+    for i in "${ROBOT[@]}"; do
+      EXTRA_ARGS+=(-d "robot=${i}")
+    done
+fi
+
+###If tag not provided, generate using Campaign Name, Commiter and UnixTimestamp
+if [[ "$TAG" != "" ]]; 
+then
+    echo "Override Tag: $TAG";
+    EXTRA_ARGS+=(-d "tag=$TAG")
+else
+    tag=$CAMPAIGN.$AUTHOR.$(date +%s)
+    EXTRA_ARGS+=(-d "tag=$tag")
+fi
+
 
 ###Run Campaign
-curl -s --request POST --url "$HOST/AddToExecutionQueueV003" -d campaign=$CAMPAIGN -d tag=$tag -H "apikey:$APIKEY"
+curl -s --request POST --url "$HOST/AddToExecutionQueueV003" -d campaign=$CAMPAIGN "${EXTRA_ARGS[@]}" -H "apikey:$APIKEY"
 echo
 
 ###Loop on resultCI Until end of campaign
