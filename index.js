a7ec16a23de364c4770d9b7859aef1017469aadf
Benoit DUMONT <bdumont@cerberus-testing.com>
2025-07-13 15:19:10 +0200

    increase default max duration to 25 min. Added 2 parameters in order to tune the max number of iteration and duration between 2 controls.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const os           = require('os');
-const { execSync } = require('child_process');
-const { getInput } = require('@actions/core');
+const os = require('os');
+const {execSync} = require('child_process');
+const {getInput} = require('@actions/core');
 
-function log (message) {
+function log(message) {
     console.log(message);
 }
 
-function getInputStr (argValue) {
+function getInputStr(argValue) {
     if (!argValue)
         return 'not specified';
 
@@ -17,18 +17,22 @@ const author = getInput('author');
 const host = getInput('host');
 const campaign = getInput('campaign');
 const apikey = getInput('apikey');
-const country = getInput('country')==="" ? "" : "-C " + getInput('country');
-const environment = getInput('environment')==="" ? "" : "-E " + getInput('environment');
-const robot = getInput('robot')==="" ? "" : "-R " + getInput('robot');
-const tag = getInput('tag')==="" ? "" : "-T " + getInput('tag');
-       
-const gitCloneCmd    = `git clone https://github.com/cerberustesting/cerberus-cicd-github-action.git`;
-const chmodCmd    = `chmod +x cerberus-cicd-github-action/launchTest.sh`;
-const launchTestCmd = `cerberus-cicd-github-action/launchTest.sh -a ${author} -h ${host} -c ${campaign} -k ${apikey} ${country} ${environment} ${robot} ${tag}`;
+const iterationnb = getInput('iterationnb') === "" ? "" : "-t " + getInput('iterationnb');
+const iterationdur = getInput('iterationdur') === "" ? "" : "-u " + getInput('iterationdur');
+const country = getInput('country') === "" ? "" : "-C " + getInput('country');
+const environment = getInput('environment') === "" ? "" : "-E " + getInput('environment');
+const robot = getInput('robot') === "" ? "" : "-R " + getInput('robot');
+const tag = getInput('tag') === "" ? "" : "-T " + getInput('tag');
+
+const gitCloneCmd = `git clone https://github.com/cerberustesting/cerberus-cicd-github-action.git`;
+const chmodCmd = `chmod +x cerberus-cicd-github-action/launchTest.sh`;
+const launchTestCmd = `cerberus-cicd-github-action/launchTest.sh -a ${author} -h ${host} -c ${campaign} -k ${apikey} ${iterationnb} ${iterationdur} ${country} ${environment} ${robot} ${tag}`;
 
 log(`AUTHOR: ${getInputStr(author)}`);
 log(`HOST: ${getInputStr(host)}`);
 log(`CAMPAIGN: ${getInputStr(campaign)}`);
+log(`ITERATIONNB: ${getInputStr(iterationnb)}`);
+log(`ITERATIONDUR: ${getInputStr(iterationdur)}`);
 log(`OVERRIDE COUNTRY: ${getInputStr(country)}`);
 log(`OVERRIDE ENVIRONMENT: ${getInputStr(environment)}`);
 log(`OVERRIDE ROBOT: ${getInputStr(robot)}`);
@@ -37,12 +41,12 @@ log(`OVERRIDE TAG: ${getInputStr(tag)}`);
 
 log('clone project...');
 log(gitCloneCmd);
-execSync(gitCloneCmd, { stdio: 'inherit' });
+execSync(gitCloneCmd, {stdio: 'inherit'});
 
 log('Changing Rights...');
 log(chmodCmd);
-execSync(chmodCmd, { stdio: 'inherit' });
+execSync(chmodCmd, {stdio: 'inherit'});
 
 log('Executing script...');
 log(launchTestCmd);
-execSync(launchTestCmd, { stdio: 'inherit' });
+execSync(launchTestCmd, {stdio: 'inherit'});
--- launchTest.sh
+++ launchTest.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ###Get CampaignName and Cerberus Host
-while getopts a:h:c:k:C:E:R:T: flag
+while getopts a:h:c:k:t:u:d:C:E:R:T: flag
 do
     case "${flag}" in
         a) AUTHOR=${OPTARG};;
         h) HOST=${OPTARG};;
         c) CAMPAIGN=${OPTARG};;
         k) APIKEY=${OPTARG};;
+        t) TIMEOUT=${OPTARG};;
+        u) ITDUR=${OPTARG};;
+        d) DEBUG=" -v";;
         C) set -f
            IFS=,
            COUNTRY=($OPTARG);;
@@ -25,6 +28,29 @@ EXTRA_ARGS=()
 echo "Author: $AUTHOR";
 echo "Cerberus Host: $HOST";
 echo "Campaign Name: $CAMPAIGN";
+#echo "Debug: $DEBUG";
+
+###If Iteration Duration not provided, default to 3
+if [[ "$ITDUR" == "" ]]; 
+then
+    ITDUR=3;
+fi
+
+###If Timeout Iteration nb not provided, default to 300
+if [[ "$TIMEOUT" != "" ]]; 
+then
+    echo "Timeout: $TIMEOUT iterations of $ITDUR sec";
+else
+    TIMEOUT=500;
+    echo "Timeout (default): $TIMEOUT iterations of $ITDUR sec";
+fi
+
+###Control host value
+if [[ "$HOST" == "" ]]; 
+then
+    echo "HOST not defined!!";
+    exit 1;
+fi
 
 if [[ "$COUNTRY" != "" ]]; then
     echo "Override Country: ${COUNTRY[@]}";
@@ -57,26 +83,60 @@ fi
 EXTRA_ARGS+=(-d "tag=$tag")
 
 ###Run Campaign
-curl -s --request POST --url "$HOST/AddToExecutionQueueV003" -d campaign=$CAMPAIGN "${EXTRA_ARGS[@]}" -H "apikey:$APIKEY"
-echo
-sleep 3
+echo "Trigger Campaign '$CAMPAIGN' to '$HOST/AddToExecutionQueueV003' with tag '$TAG'"
+callTrigger=$(curl -s $DEBUG --request POST --url "$HOST/AddToExecutionQueueV003" -d campaign=$CAMPAIGN -d outputformat=json "${EXTRA_ARGS[@]}" -H "apikey:$APIKEY")
+callTriggerRC=$(echo $callTrigger | jq -r '.messageType')
+callTriggerMess=$(echo $callTrigger | jq -r '.message')
+
+if [[ "$callTriggerRC" != "OK" ]]; then
+    echo "Could not trigger the campaign!!"
+    echo $callTriggerMess
+    exit 1
+else
+    echo $callTriggerMess
+fi
+
+
+#echo $callTrigger
 
 ###Loop on resultCI Until end of campaign
 num=1
-while [ $num -lt 300 ]
+while [ $num -le $TIMEOUT ]
 do
-    result=$(curl -s --request POST --url "$HOST/ResultCIV004" -d tag=$tag -H "apikey:$APIKEY"| jq -r '.result')
-    echo "Check on Campaign ($num/300) with result : " $result
+    sleep $ITDUR
+## Get the tag result.
+    resultFull=$(curl -s $DEBUG --request POST --url "$HOST/ResultCIV004" -d tag=$tag -H "apikey:$APIKEY")
+## Parsing call result
+    result=$(echo $resultFull | jq -r '.result')
+    resultMess=$(echo $resultFull | jq -r '.message')
+    resultTot=$(echo $resultFull | jq -r '.TOTAL_nbOfExecution')
+    resultQU=$(echo $resultFull | jq -r '.status_QU_nbOfExecution')
+    resultPE=$(echo $resultFull | jq -r '.status_PE_nbOfExecution')
+    resultOK=$(echo $resultFull | jq -r '.status_OK_nbOfExecution')
+    resultKO=$(echo $resultFull | jq -r '.status_KO_nbOfExecution')
+    resultFA=$(echo $resultFull | jq -r '.status_FA_nbOfExecution')
+    resultCIR=$(echo $resultFull | jq -r '.CI_finalResult')
+    resultCIT=$(echo $resultFull | jq -r '.CI_finalResultThreshold')
+    echo $(date +%y-%m-%d" "%H:%M:%S)" Check Campaign result ($num/$TIMEOUT) | QU:"$resultQU" PE:"$resultPE" | OK:"$resultOK" KO:"$resultKO" FA:"$resultFA 
+## Exit if result is no longuer pending
     if [[ "$result" != "PE" ]]; then
         break
     fi
-    sleep 3
     ((num=num+1))
 #    echo "Campaign still running... Let's try again."
 done
 
-if [[ "$result" != "OK" ]]; then
-    echo "Campaign Failed. CIScore Higher than threshold !!!"
+## Timeout reached!!!
+if [[ $num -ge $TIMEOUT ]]; then
+    echo "Maximum iteration checks reached!!!"
     exit 1
 fi
+
+## Result fail!!!
+if [[ "$result" != "OK" ]]; then
+    echo "Campaign Failed. CIScore $resultCIR is above threshold $resultCIT!!!"
+    exit 1
+fi
+
+## Result OK
 echo "Campaign Successful. Congratulation !!!"
